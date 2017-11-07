b9d9a3ecabf9b398b07425ea1251a4b5d71bf6c4
Janitha Tennakoon <janitha000@gmail.com>
2017-11-07 08:31:54 +0530

    Added folder structures

---
+++ REST/REST.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace REST.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ REST/REST.Library/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace REST.Library
+{
+    public class Class1
+    {
+    }
+}
